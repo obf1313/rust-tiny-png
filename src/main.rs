8a9f94cfbb82f12ad579fdc45537170f268ab311
obf1313 <oubaifan@163.com>
2024-04-19 20:52:39 +0800

    feat: 接收文件，压缩后导出文件

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate image;
+use image::imageops::FilterType;
+use image::GenericImageView;
+
 fn main() {
-    println!("Hello, world!");
+    let img = image::open("input.jpg").expect("Failed to open image");
+
+    let width = img.width();
+    let height = img.height();
+
+    let resized_img = img.resize(width, height, FilterType::Lanczos3);
+    resized_img
+        .save("output.jpg")
+        .expect("Failed to save image");
 }

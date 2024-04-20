660fae2f42806e7eee6925393958de11e33c2c3c
obf1313 <oubaifan@163.com>
2024-04-20 13:31:28 +0800

    feat: 导出 wasm 文件

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
+// TODO: 这是啥意思
 extern crate image;
-use std::io::Cursor;
-
 use image::imageops::FilterType;
+use std::io::Cursor;
+use wasm_bindgen::prelude::*;
 
 /**
  * 压缩图片
  */
+#[wasm_bindgen]
 pub fn tiny_png(file_buffer: &[u8]) -> Vec<u8> {
     // 使用字节数组初始化图像
     let img = image::load_from_memory(file_buffer).expect("Failed to load image");
@@ -20,3 +22,5 @@ pub fn tiny_png(file_buffer: &[u8]) -> Vec<u8> {
         .expect("Failed to write image to buffer");
     return buffer;
 }
+// 执行以下命令可以打包
+// wasm-pack build --target web --out-name rust_tiny_png

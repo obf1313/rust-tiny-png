b41379a3326f64c864c1d1a22911692892ce6172
obf1313 <oubaifan@163.com>
2024-04-21 14:23:38 +0800

    feat: 测试用例

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 // TODO: 这是啥意思
 extern crate image;
 use image::imageops::FilterType;
+use std::fs::File;
+use std::io::prelude::*;
 use std::io::Cursor;
 use wasm_bindgen::prelude::*;
 
@@ -24,3 +26,24 @@ pub fn tiny_png(file_buffer: &[u8]) -> Vec<u8> {
 }
 // 执行以下命令可以打包
 // wasm-pack build --target web --out-name rust_tiny_png
+
+// 运行 cargo test 测试
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_size() {
+        let mut file = File::open("input.jpg").expect("Failed to open image file");
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer)
+            .expect("Failed to read image file");
+        let out_buffer = tiny_png(&buffer);
+        // 将字节缓冲区写入文件
+        let mut new_file = File::create("output.jpg").expect("Failed to create output file");
+        new_file
+            .write_all(&out_buffer)
+            .expect("Failed to write buffer to file");
+        assert!(buffer.len() > out_buffer.len());
+    }
+}

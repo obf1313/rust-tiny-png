a84bb2aaf0a4303099dc69c9ab8853fa158f1689
obf1313 <chenna2@sisyphe.com.cn>
2024-04-22 17:14:25 +0800

    feat: 新增测试用例、以及发现了 bug

--- src/lib.rs
+++ src/lib.rs
@@ -10,17 +10,22 @@ use wasm_bindgen::prelude::*;
  * 压缩图片
  */
 #[wasm_bindgen]
-pub fn tiny_png(file_buffer: &[u8]) -> Vec<u8> {
+pub fn tiny_png(file_buffer: &[u8], file_type: &str) -> Vec<u8> {
     // 使用字节数组初始化图像
-    let img = image::load_from_memory(file_buffer).expect("Failed to load image");
-    let width = img.width();
-    let height = img.height();
-    // 压缩图片
-    let resized = img.resize(width, height, FilterType::Lanczos3);
+    let img: image::DynamicImage =
+        image::load_from_memory(file_buffer).expect("Failed to load image");
+    let format = match file_type {
+        // 正常
+        "image/jpeg" => image::ImageFormat::Jpeg,
+        // FIXME: 变大了
+        "image/png" => image::ImageFormat::Png,
+        // FIXME: 不动了
+        "image/gif" => image::ImageFormat::Gif,
+        _ => image::ImageFormat::Jpeg,
+    };
     // 初始化字节流
     let mut buffer = Vec::new();
-    resized
-        .write_to(&mut Cursor::new(&mut buffer), image::ImageFormat::Jpeg)
+    img.write_to(&mut Cursor::new(&mut buffer), format)
         .expect("Failed to write image to buffer");
     return buffer;
 }
@@ -32,18 +37,32 @@ pub fn tiny_png(file_buffer: &[u8]) -> Vec<u8> {
 mod tests {
     use super::*;
 
-    #[test]
-    pub fn test_size() {
-        let mut file = File::open("input.jpg").expect("Failed to open image file");
+    fn resize(in_name: &str, out_name: &str, file_type: &str) {
+        let mut file = File::open(in_name).expect("Failed to open image file");
         let mut buffer = Vec::new();
         file.read_to_end(&mut buffer)
             .expect("Failed to read image file");
-        let out_buffer = tiny_png(&buffer);
+        let out_buffer = tiny_png(&buffer, file_type);
         // 将字节缓冲区写入文件
-        let mut new_file = File::create("output.jpg").expect("Failed to create output file");
+        let mut new_file = File::create(out_name).expect("Failed to create output file");
         new_file
             .write_all(&out_buffer)
             .expect("Failed to write buffer to file");
         assert!(buffer.len() > out_buffer.len());
+    }
+
+    #[test]
+    pub fn test_jpg() {
+        resize("input.jpg", "output.jpg", "image/jpeg");
+    }
+
+    #[test]
+    pub fn test_png() {
+        resize("input.png", "output.png", "image/png");
+    }
+
+    #[test]
+    pub fn test_gif() {
+        resize("input.gif", "output.gif", "image/gif");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use std::io::prelude::*;
 
 fn main() {
     // 读取图像文件到字节数组
-    let mut file = File::open("input.jpg").expect("Failed to open image file");
+    let mut file = File::open("input.png").expect("Failed to open image file");
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer)
         .expect("Failed to read image file");
-    let out_buffer = tiny_png(&buffer);
+    let out_buffer = tiny_png(&buffer, "image/png");
     // 将字节缓冲区写入文件
-    let mut file = File::create("output.jpg").expect("Failed to create output file");
+    let mut file = File::create("output.png").expect("Failed to create output file");
     file.write_all(&out_buffer)
         .expect("Failed to write buffer to file");
 }
